f1356fbc8485d201c0aa037f0827a53259e99a0d
tiffanylinda <tl108@wellesley.edu>
2025-12-17 11:23:51 -0500

    update

--- 2024_Songs.py
+++ 2024_Songs.py
@@ -32,7 +32,7 @@ with intro:
     # --- Introduction ---
     st.header("Introduction")
     st.write("""
-             It's the year 2024, and another popular artist such as Taylor Swift has released their new album, **The Tortured Poets Department**.
+             It's the year 2024, and yet again another popular artist such as Taylor Swift has released their new album, **The Tortured Poets Department**.
             We expect to see a lot of buzz and attention directed towards the release of this album. 
             In order to investigate how interested the public is in this album, we might decide to go look at the number of pageviews
             that the album is accumulating. A lot of internet users like to use Wikipedia as a source of information, so we decide to 
@@ -53,13 +53,12 @@ with intro:
     st.write("3. How much attention was on popular artists compared to smaller artists?")
 
     st.write("""My preliminary assumptions lead me to believe that the most popular artists will be artists 
-             such as Taylor Swift, Beyonce, etc. 
+             such as Taylor Swift, Beyonce, etc. """)
              
-             The most popular songs will most likely be from the top/popular artists.
+    st.write("The most popular songs will most likely be from the top/popular artists.")         
 
-             We might see about 80 percent of the total pageview data being dominated by the top artists.
-             
-             """)
+    st.write("We might see about 80 percent of the total pageview data being dominated by the top artists.")        
+
 
 with data_summary:
     st.header("Data Summary")
@@ -221,12 +220,13 @@ with hypothesis:
     artist_summary.columns = ['artist', 'total_pageviews']
     artist_summary = artist_summary.sort_values(by='total_pageviews', ascending=False).reset_index(drop=True)
 
-    # 2. **NEW: Calculate Programmatic Jump Detection**
+    # 2. Calculate Programmatic Jump Detection
 
     # 2a. Calculate the drop-off rate between sequential artists
     # Shift(-1) brings the pageviews of the *next* artist into the current artist's row
     artist_summary['next_artist_views'] = artist_summary['total_pageviews'].shift(-1)
     artist_summary['view_drop'] = artist_summary['total_pageviews'] - artist_summary['next_artist_views']
+
     # Calculate the percentage drop from the current artist's views to the next artist's views
     artist_summary['pct_drop'] = (artist_summary['view_drop'] / artist_summary['total_pageviews']) * 100
 
@@ -234,7 +234,8 @@ with hypothesis:
     artist_summary_analysis = artist_summary.dropna(subset=['next_artist_views']).copy() 
 
     # 2b. Define and Identify the Jump
-    JUMP_THRESHOLD_PCT = 60 # Set a threshold (e.g., a drop of 60% or more to the next artist)
+    # Setting a threshold of 60 percent
+    JUMP_THRESHOLD_PCT = 60 
 
     # Find the first major separation point
     jump_point = artist_summary_analysis[artist_summary_analysis['pct_drop'] >= JUMP_THRESHOLD_PCT].head(1)
@@ -292,7 +293,7 @@ with hypothesis:
 
     # Add a marker for the jump index if it's within the selected Top N
     color_condition = alt.condition(
-    # This check is now safe because jump_artist is guaranteed to be a string ("" if not found)
+    # jump_artist is guaranteed to be a string ("" if not found)
     alt.datum.artist == jump_artist, 
     alt.value('red'), # Color the bar red if it's the jump artist
     alt.value('#E91E63') # Otherwise use the default pink color
@@ -417,20 +418,106 @@ with visuals:
         columns={'article': 'Song Name', 'monthly_pageviews': 'Monthly Pageviews'}), 
         use_container_width=True
     )
-
     st.markdown("---")
+
+    # --- TOTAL ACCUMULATION SECTION ---
+    st.write("## 🏆 Overall Top Songs")
+
+    # 1. Aggregate and Calculate Percentages
+    df_total_stats = data.groupby('article')['monthly_pageviews'].sum().reset_index()
+    df_total_stats.rename(columns={'monthly_pageviews': 'total_pageviews'}, inplace=True)
+
+    # Calculate the % share of total traffic for each song
+    total_site_views = df_total_stats['total_pageviews'].sum()
+    df_total_stats['percent_share'] = (df_total_stats['total_pageviews'] / total_site_views) * 100
+
+    # 2. Controls
+    col_acc_1, col_acc_2 = st.columns([1, 1])
+
+    with col_acc_1:
+        total_n = st.slider(
+            "Show Top N Songs Overall:",
+            min_value=5,
+            max_value=min(100, len(df_total_stats)),
+            value=15,
+            key='total_n_slider_final'
+        )
+
+    with col_acc_2:
+        total_sort = st.selectbox(
+            "Sort Overall By:",
+            options=['Total Pageviews (High to Low)', 'Song Name (A-Z)'],
+            key='total_sort_final'
+        )
+
+    # 3. Sorting Logic (Highest Views always stay on top unless A-Z is chosen)
+    if total_sort == 'Song Name (A-Z)':
+        df_display_total = df_total_stats.nlargest(total_n, 'total_pageviews').sort_values('article')
+        y_sort_acc = None
+    else:
+        df_display_total = df_total_stats.nlargest(total_n, 'total_pageviews')
+        y_sort_acc = alt.EncodingSortField(field="total_pageviews", order='descending')
+
+    # 4. Visualization
+    chart_total = alt.Chart(df_display_total).mark_bar(color='#29b5e8').encode(
+        y=alt.Y('article:N', sort=y_sort_acc, title=None),
+        x=alt.X('total_pageviews:Q', title='Total Accumulated Pageviews', axis=alt.Axis(format='~s')),
+        tooltip=[
+            alt.Tooltip('article', title='Song'),
+            alt.Tooltip('total_pageviews', title='Total Views', format=','),
+            alt.Tooltip('percent_share', title='% of Total Traffic', format='.2f') # Added percentage to tooltip
+        ]
+    ).properties(
+        title="Cumulative Song Performance",
+        height=400
+    ).interactive()
+
+    st.altair_chart(chart_total, use_container_width=True)
+
+    # 5. Raw Data with Percentage formatting
+    st.write("### Data Summary")
+    st.dataframe(
+        df_display_total.rename(columns={
+            'article': 'Song Name', 
+            'total_pageviews': 'Total Views',
+            'percent_share': '% Share'
+        }),
+        use_container_width=True,
+        column_config={
+            "% Share": st.column_config.NumberColumn(format="%.2f%%") # Formats as 5.25%
+        }
+    )
 
 
 with summary:
     st.header("Summary & Ethical Considerations ")
     st.write("""
     The key takeaways from my investigation show that the top artists are: 
-    The top songs of 2024 are:.
-    About something percent of attention is directed towards popular artists vs smaller artists.
+             1. Taylor Swift
+             2. The Beatles
+             3. Eminem
+             4. Sabrina Carpenter
+             5. Fleetwood Mac
+             6. Kendrick Lamar
+             7. Connie Francis
+             8. Beyonce
+             9. Billie Eilish
+             10. Usher
+    The top songs of 2024 are:
+             
+    These Top 10 Artists Account for 14.88 percent of the total accumulated pageviews.
+    
+    The most interesting find is the big drop between Taylor Swift and the next person.
+    
+    Taylor makes up 3.89 percent of the total accumulated pageviews in 2024. 
+             
+    Therefore, that means that the rest of the artists make up 96.11 percent of the rest of the accumulated pageviews in 2024.
 
-    I'm am fairly confident that the results are reliable because…
+    I'm fairly confident that the results are for the most part reliable because the processes I took in order to make sure the songs and QID were properly processed.
+    This also makes sense because in recent times, pop artists especially Taylor Swift have been dominating the music industry. 
 
     I used zero-shot classification, but was not able to assess the model's accuracy as I did not have ground truth data.
+    One possible path I could do moving foward is using the lyric data to predict genre because I have ground truth for genre.
 
     There isn't any major ethical concerns as the data was collected from public sources. 
 
